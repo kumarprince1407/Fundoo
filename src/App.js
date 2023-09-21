63c059a426ff1c0780e77a61b9f800e705fe0212
Prince Kumar <kumarprince1407@gmail.com>
2023-09-21 14:58:39 +0530

    Update 1 for Fundoo project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import PrimarySearchAppBar from "./components/Search";
-import MiniDrawer from "./components/Drawer";
+
 import MiniDrawer2 from "./components/Drawer2";
 import Dashboard from "./components/Dashboard";
 import Router from "./router/Router";
